c219f76121e8432cb48ce90eb7d8cc627322d42e
Ugur Dal <ugurdal@derinbilgi.com.tr>
2021-05-02 12:43:31 +0300

    trendyol fake order api

--- employees.js
+++ employees.js
@@ -1,18 +1,20 @@
 // employees.js
-var faker = require('faker')
-function generateEmployees () {
-  var employees = []
+var faker = require("faker");
+
+function generateEmployees() {
+  var employees = [];
   for (var id = 0; id < 50; id++) {
-    var firstName = faker.name.firstName()
-    var lastName = faker.name.lastName()
-    var email = faker.internet.email()
+    var firstName = faker.name.firstName();
+    var lastName = faker.name.lastName();
+    var email = faker.internet.email();
+
     employees.push({
-      "id": id,
-      "first_name": firstName,
-      "last_name": lastName,
-      "email": email
-    })
+      id: id,
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+    });
   }
-  return { "employees": employees }
+  return { employees: employees };
 }
-module.exports = generateEmployees
+module.exports = generateEmployees;
